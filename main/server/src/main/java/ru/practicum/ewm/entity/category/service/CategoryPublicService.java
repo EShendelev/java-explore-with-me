75839f9694835de9e9141820d75f64c682b80cfb
EShendelev <sendeleve@yandex.ru>
2024-01-15 20:02:29 +0300

    main stage

--- main/server/src/main/java/ru/practicum/ewm/entity/category/service/CategoryPublicService.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.ewm.entity.category.service;
-
-import ru.practicum.ewm.entity.category.dto.response.CategoryResponseDto;
-
-public interface CategoryPublicService {
-    Iterable<CategoryResponseDto> getCategories(Integer from, Integer size);
-
-    CategoryResponseDto getCategoryById(Long catId);
-}
